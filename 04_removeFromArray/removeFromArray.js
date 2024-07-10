91abc5a66b9a3246ab629dae5094c1a274d2856c
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-07-10 12:32:44 -0400

    Complete problem 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    const container = [...args];
+    for (let i = 0; i < container.length; i++) {
+        while (arr.indexOf(container[i]) >= 0) {
+            arr.splice(arr.indexOf(container[i]),1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
