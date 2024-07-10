89c3ec0c3379f3784d67f3ad08dbb24186fe13f3
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-07-10 12:48:35 -0400

    Complete problem 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+    // both numbers
+    if ((typeof x != ("number" || "bigint")) || (typeof y != ("number" || "bigint"))) {
+        return 'ERROR';
+    }
+    // negative 
+    if (x < 0 || y < 0) {return 'ERROR'}
+    //find smaller
+    var sum = 0;
+    var small;
+    var big;
+    if (x>y) {
+        big = x;
+        small = y;
+    }
+    else {
+        big = y;
+        small = x;
+    }
+    for (let i = small; i <= big; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
