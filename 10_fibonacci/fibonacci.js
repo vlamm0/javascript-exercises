c91edb62f8e097f20135fbad05c8fb128c57428b
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-07-12 18:06:47 -0400

    Complete problem 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    // convert str to num
+    num = Number(num);
+
+    // deal with zero
+    if (num == 0) {return 0};
+
+    // deal with negatives
+    if (num < 0) {return "OOPS"}
+
+    let fib = [1,1];
+    for (let i = 2; i < num; i++) {
+        fib.push(fib[i-2]+ fib[i-1])    
+    }
+    
+    return fib[num-1]
+
 
 };
 
