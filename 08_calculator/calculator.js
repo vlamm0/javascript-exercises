7e455933be26c6f22129288fed8d4cbcce8c8970
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-07-12 16:36:39 -0400

    Complete problem 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, num) => {
+    return total + num;
+  }, 0)
+
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, num) => {
+    return total * num;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0) {return 1}
+  let cap = 1;
+  for (let i = 1; i <= num; i++) {
+    cap = cap * i;
+  } return cap;
 };
 
 // Do not edit below this line
