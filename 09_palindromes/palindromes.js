b2de226f5d108a7a2a17371fbc149d033266f96e
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-07-12 17:51:50 -0400

    Complete problem 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // split str into array of letters
+    const regex = /[a-zA-Z0-9]/
+    let characters = str.split("")
+    characters = characters.map((char) => char.toUpperCase())
+    let forward = characters.filter((char) => (regex.test(char)))
 
+    // reverse characters
+    let copy = forward.slice(0,)
+    let reverse = copy.reverse()
+
+    // test every character in both
+    for (let i = 0; i < forward.length; i++) {
+        if (forward[i] != reverse[i]) {
+            return false
+        }
+    } return true
 };
-
 // Do not edit below this line
 module.exports = palindromes;
