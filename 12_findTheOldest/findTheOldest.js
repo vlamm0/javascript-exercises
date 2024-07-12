a15e0e0274b4297152de638db9bdb0f4fbb8f5b3
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-07-12 19:46:57 -0400

    Complete problem 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,36 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    // const people = [
+    //     {
+    //       name: "Carly",
+    //       yearOfBirth: 1942,
+    //       yearOfDeath: 1970,
+    //     },
+    //     {
+    //       name: "Ray",
+    //       yearOfBirth: 1962,
+    //       yearOfDeath: 2011,
+    //     },
+    //     {
+    //       name: "Jane",
+    //       yearOfBirth: 1912,
+    //       yearOfDeath: 1941,
+    //     },
+    // ]
+    
+    people.sort(function(a,b) {
+        let aAge = a.yearOfDeath - a.yearOfBirth;
+        let bAge = b.yearOfDeath - b.yearOfBirth;
+        let date = new Date().getFullYear()
+        if (!a.hasOwnProperty("yearOfDeath")) {
+            aAge = date - a.yearOfBirth;
+        }
+        if (!b.hasOwnProperty("yearOfDeath")) {
+            bAge = date - b.yearOfBirth;
+        }
+        return aAge > bAge ? -1 : 1;
+    })
+    console.table(people)
+    return people[0]
 
 };
 
